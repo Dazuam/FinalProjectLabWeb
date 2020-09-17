22def0472941914c770d4825a79021f9f3e8d5f8
NicoleBarra <38593156+NicoleBarra@users.noreply.github.com>
2020-09-17 18:52:19 -0500

    login and sign up

--- routes/web.js
+++ routes/web.js
@@ -2,8 +2,19 @@ let router = require('express').Router();
 let authController = require('../controllers/AuthController');
 let authValidator = require('../validators/AuthValidators');
 let passport = require('passport');
-const {userAuthCheck} = require('../checkAuth');
 
+// Authentication routes
+router.get('/login', authController.login);
+router.get('/register', authController.register);
+
+router.post('/register', authValidator.store, authController.store);
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login-fail', successRedirect: '/protected' }));
+router.get('/protected', (req, res) => {
+  res.send('Usuario logueado con éxito');
+});
+router.get('/login-fail', (req, res) => {
+  res.send('El usuario no tiene una sesión válida');
+});
 
 
 
