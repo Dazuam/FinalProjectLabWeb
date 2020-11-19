99966b3954f68225566955b62caa5712ab77cde4
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-11-19 12:45:38 -0600

    Style part 3

--- models/Upload.js
+++ models/Upload.js
@@ -30,5 +30,5 @@ exports.create = (upload, image, user) => {
 
   let url = 'http://localhost:3000/images/' + image.filename;
   return knex('uploads')
-    .insert({ title: upload.title, description: upload.desciption, imgUrl: url, user: user.name })
+    .insert({ title: upload.title, description: upload.description, imgUrl: url, user: user.name })
 }
