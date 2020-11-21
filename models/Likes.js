e49887f86d1268b2827ee0482b51cdc2225c7db1
Dazuam <dazuam2@gmail.com>
2020-11-20 19:46:47 -0600

    Merge

--- models/Likes.js
+++ models/Likes.js
@@ -1,9 +1,8 @@
 const knex = require('../database/connection');
 
-exports.addLike = (post_id, user_id) => {
-    return knex('likes')
+exports.addLike = (post_id) => {
+    return knex('uploads')
     .insert({post_id: post_id, user_id: user_id})
-
 }
 
 exports.likesTimeline = (id) => {
