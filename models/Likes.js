9982b6347903b1ec40d49f7f9a5400c0f65a80df
Dazuam <dazuam2@gmail.com>
2020-11-20 18:49:01 -0600

    imagedetail works

--- models/Likes.js
+++ models/Likes.js
@@ -1,8 +1,21 @@
 const knex = require('../database/connection');
 
-exports.findForTimeline = (id) => {
+exports.addLike = (post_id, user_id) => {
+    return knex('likes')
+    .insert({post_id: post_id, user_id: user_id})
+
+}
+
+exports.likesTimeline = (id) => {
     return knex
-      .select('*')
-      .from('likes')
-      .whereIn('user_id', id)
-  }
+        .select('*')
+        .from('likes')
+        .where('user_id', id)
+}
+
+exports.numberOfLikes = (id) => {
+    return knex
+    .select('*')
+    .from('likes')
+    .whereIn('post_id', id)
+}
